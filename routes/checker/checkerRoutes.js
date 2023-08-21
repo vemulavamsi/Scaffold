dc2ebb0b8e67f09c83e1e4c06c58030cf84438b5
Vamsi_vemula <vamsi.v@pycube.com>
2023-08-21 01:03:41 -0400

    minor

--- routes/checker/checkerRoutes.js
+++ routes/checker/checkerRoutes.js
@@ -10,7 +10,7 @@ const logger = require('../../logger/logger')
  */
 const sayHello = (req, res) => {
     logger.info(`Checker route! Saying hello from ${req.originalUrl}`)
-    res.status(200).json({ "message": "Hello vamsi vemula! I am ready for work 08-19-2023!!!@@@" })
+    res.status(200).json({ "message": "Hello vamsi vemula! I am ready for work 08-21-2023!!!@@@" })
 }
 
 module.exports = {
